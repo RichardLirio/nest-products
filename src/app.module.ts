46dc130af5b88baae0e7b08bf1b91328f6e1bdda
Richard Lirio <richardlirio@hotmail.com>
2025-07-18 21:59:57 -0300

    chore: add swagger and controller products

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
-import { ProductsModule } from './products/products.module';
+import { ProductsModule } from "./products/products.module";
+import { ProductsController } from "./products/products.controller";
 
 @Module({
   imports: [ProductsModule],
-  controllers: [],
+  controllers: [ProductsController],
   providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,25 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // Habilitar CORS
+  app.enableCors();
+
+  // Swagger/OpenAPI
+  const config = new DocumentBuilder()
+    .setTitle("Products API")
+    .setDescription("API para gerenciamento de produtos")
+    .setVersion("1.0")
+    .addTag("products")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
+
+  await app.listen(3333);
+
+  console.log("🚀 Servidor rodando na porta 3333");
+  console.log("📚 Documentação Swagger: http://localhost:3333/docs");
 }
 bootstrap();
