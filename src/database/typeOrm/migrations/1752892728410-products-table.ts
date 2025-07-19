fc2c4fb33aa263a669c3669b7b90e9e5f671a374
Richard Lirio <richardlirio@hotmail.com>
2025-07-18 23:53:18 -0300

    refactor: migrations sql script for test refactor

--- src/database/typeOrm/migrations/1752892728410-products-table.ts
+++ src/database/typeOrm/migrations/1752892728410-products-table.ts
@@ -1,14 +1,29 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class ProductsTable1752892728410 implements MigrationInterface {
-    name = 'ProductsTable1752892728410'
+  name = "ProductsTable1752892728410";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "products" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" text NOT NULL, "price" real NOT NULL, "sku" text NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_c44ac33a05b144dd0d9ddcf9327" UNIQUE ("sku"), CONSTRAINT "PK_0806c755e0aca124e67c0cf6d7d" PRIMARY KEY ("id"))`);
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const schema =
+      new URL(process.env.DATABASE_URL!).searchParams.get("schema") || "public";
+    // Verifica se a tabela já existe
+    const tableExists = await queryRunner.hasTable(`"${schema}"."users"`);
+    if (!tableExists) {
+      await queryRunner.query(
+        ` CREATE TABLE "${schema}"."products" (
+         "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+         "name" text NOT NULL, "price" real NOT NULL,
+         "sku" text NOT NULL,
+         "created_at" TIMESTAMP NOT NULL DEFAULT now(), 
+         "updated_at" TIMESTAMP NOT NULL DEFAULT now(),
+         CONSTRAINT "UQ_c44ac33a05b144dd0d9ddcf9327" UNIQUE ("sku"), CONSTRAINT "PK_0806c755e0aca124e67c0cf6d7d" PRIMARY KEY ("id"))`
+      );
     }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "products"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const schema =
+      new URL(process.env.DATABASE_URL!).searchParams.get("schema") || "public";
+    await queryRunner.query(`DROP TABLE IF EXISTS "${schema}"."products"`);
+  }
 }
